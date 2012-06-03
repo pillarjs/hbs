5c0021e99fc1a7c8a7c0ba779aaeb05ba46b9be8
Roman Shtylman <shtylman@gmail.com>
2012-06-03 00:22:25 -0400

    update 3.x test to 3.0.0beta1

--- test/3.x/index.js
+++ test/3.x/index.js
@@ -3,7 +3,7 @@ var npm = require("npm");
 test('setup3.x', function(done) {
     npm.load(function(err, npm) {
         npm.localPrefix = __dirname;
-        npm.commands.install(['express@>=3.0.0alpha2'], function() {
+        npm.commands.install(['express@>=3.0.0beta1'], function() {
             // run tests
             require('./app');
             done();
