e39a9328f3f41eec803e4d51e4497ad697c86db0
Roman Shtylman <shtylman@gmail.com>
2012-06-26 20:54:42 -0400

    update travis.yml to include 0.8
    - update mocha dep
    - pin down express testing versions

--- test/2.x/index.js
+++ test/2.x/index.js
@@ -3,7 +3,7 @@ var npm = require("npm");
 test('setup2.x', function(done) {
     npm.load(function(err, npm) {
         npm.localPrefix = __dirname;
-        npm.commands.install(['express@>=2.5.9'], function() {
+        npm.commands.install(['express@=2.5.10'], function() {
             // run tests
             require('./app');
             done();
--- test/3.x/index.js
+++ test/3.x/index.js
@@ -3,7 +3,7 @@ var npm = require("npm");
 test('setup3.x', function(done) {
     npm.load(function(err, npm) {
         npm.localPrefix = __dirname;
-        npm.commands.install(['express@>=3.0.0beta1'], function() {
+        npm.commands.install(['express@=3.0.0beta4'], function() {
             // run tests
             require('./app');
             done();
