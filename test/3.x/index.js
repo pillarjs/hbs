6a7d738ffe3cca13c1a4c3df88069cf40c8efd53
Roman Shtylman <shtylman@gmail.com>
2013-02-21 10:55:58 -0500

    update tests to use latest express 2.x and 3.x always

--- test/3.x/index.js
+++ test/3.x/index.js
@@ -3,7 +3,7 @@ var npm = require("npm");
 test('setup3.x', function(done) {
     npm.load(function(err, npm) {
         npm.localPrefix = __dirname;
-        npm.commands.install(['express@=3.0.x'], function() {
+        npm.commands.install(['express@~3.1.0'], function() {
             // run tests
             require('./app');
             require('./async_helpers');
