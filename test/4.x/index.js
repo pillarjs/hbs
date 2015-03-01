e264a8a134fe2e9845d48816114f3f679682611a
William Monk <will@williammonk.co.uk>
2015-03-01 13:18:54 +0000

    Added multiple views abilities for express@4.12.0

--- test/4.x/index.js
+++ test/4.x/index.js
@@ -3,7 +3,10 @@ var npm = require('npm');
 test('setup4.x', function(done) {
     npm.load(function(err, npm) {
         npm.localPrefix = __dirname;
-        npm.commands.install(['express@~4.8.6'], function() {
+        npm.commands.install(['express@4.12.0'], function(err) {
+            if (err) {
+                return done(err);
+            }
             // run tests
             require('./app');
             require('./async_helpers');
