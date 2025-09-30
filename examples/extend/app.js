2e9098a4de7915ebca92dbaa836b4f58cd82e058
Alfredo González <alfredo.arosteguy@gmail.com>
2025-09-30 20:59:24 -0300

    chore(examples): fix extend example app

--- examples/extend/app.js
+++ examples/extend/app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 const blocks = {};
 
 hbs.registerHelper('extend', function(name, context) {
-    const block = blocks[name];
+    let block = blocks[name];
     if (!block) {
         block = blocks[name] = [];
     }
